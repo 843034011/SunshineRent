358d4e1283c80191691228c6e25bafa769b7fd40
liulanbo-llb <2939499345@qq.com>
2019-09-11 14:48:15 +0800

    llb0911测试

--- rent-web/src/main/java/com/elife/RentApplication.java
+++ rent-web/src/main/java/com/elife/RentApplication.java
@@ -4,13 +4,11 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.elife.mapper")
-//@ServletComponentScan
-@Controller
+@ServletComponentScan
 public class RentApplication {
 
     public static void main(String[] args) {
