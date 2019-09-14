94d3a19709fb05114141840c021127ea25856036
liulanbo-llb <2939499345@qq.com>
2019-09-14 19:35:33 +0800

    llb20190914 没有太大改动

--- rent-controller/src/main/java/com/elife/controller/ShoppingCartController.java
+++ rent-controller/src/main/java/com/elife/controller/ShoppingCartController.java
@@ -10,7 +10,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 @Controller
 @RequestMapping("shoppingcart")
