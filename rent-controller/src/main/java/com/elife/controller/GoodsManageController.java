0b8908ef1cc5998b686e004170d5a6861736863c
薄一帆 <1029153958@qq.com>
2019-09-16 22:00:46 +0800

    修改冲突

--- rent-controller/src/main/java/com/elife/controller/GoodsManageController.java
+++ rent-controller/src/main/java/com/elife/controller/GoodsManageController.java
@@ -4,7 +4,7 @@ package com.elife.controller;
 import com.elife.pojo.RentGoods;
 import com.elife.service.impl.GoodsManageServiceImpl;
 import com.elife.vo.ResultData;
-/*import org.junit.jupiter.api.Test;*/
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
