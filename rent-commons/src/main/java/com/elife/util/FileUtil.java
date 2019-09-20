b18d7dbedb4dd3199c1a4e40c7adc86bf38b59ee
薄一帆 <1029153958@qq.com>
2019-09-20 17:42:02 +0800

    修改冲突

--- rent-commons/src/main/java/com/elife/util/FileUtil.java
+++ rent-commons/src/main/java/com/elife/util/FileUtil.java
@@ -1,17 +1,15 @@
 package com.elife.util;
 
-public class FileUtil {
+    public class FileUtil {
+        // 图片允许的后缀扩展名
+        public static String[] IMAGE_FILE_EXTD = new String[] { "png", "bmp", "jpg", "jpeg","pdf"  };
 
-    // 图片允许的后缀扩展名
-    public static String[] IMAGE_FILE_EXTD = new String[] { "jpg", "png", "bmp", "jpeg" };
-
-    public static boolean isFileAllowed(String fileName) {
-        for (String ext : IMAGE_FILE_EXTD) {
-            if (ext.equals(fileName)) {
-                return true;
+        public static boolean isFileAllowed(String fileName) {
+            for (String ext : IMAGE_FILE_EXTD) {
+                if (ext.equals(fileName)) {
+                    return true;
+                }
             }
+            return false;
         }
-        return false;
     }
-
-}
